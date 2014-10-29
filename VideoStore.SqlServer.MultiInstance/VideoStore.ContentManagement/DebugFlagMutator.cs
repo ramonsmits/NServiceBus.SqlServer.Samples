2c36d1f38776e2c3c651980a727d1f5c8dd0d253
Ramon Smits <ramon.smits@gmail.com>
2014-10-29 01:06:17 +0100

    The video store example changed to make use of multiple instance support on the SQL Transport. The example has a separate sql database for each component.

---
+++ VideoStore.SqlServer.MultiInstance/VideoStore.ContentManagement/DebugFlagMutator.cs
@@ -0,0 +1,38 @@
+﻿namespace VideoStore.Common
+{
+    using NServiceBus.Unicast.Messages;
+    using System;
+    using System.Threading;
+    using NServiceBus;
+    using NServiceBus.MessageMutator;
+
+    public class DebugFlagMutator : IMutateTransportMessages, INeedInitialization
+    {
+        public static bool Debug { get { return debug.Value; } }
+
+        public void MutateIncoming(TransportMessage transportMessage)
+        {
+            var debugFlag = transportMessage.Headers.ContainsKey("Debug") ? transportMessage.Headers["Debug"] : "false";
+            if (debugFlag !=null && debugFlag.Equals("true", StringComparison.OrdinalIgnoreCase))
+            {
+                debug.Value = true;
+            }
+            else
+            {
+                debug.Value = false;
+            }
+        }
+
+        public void MutateOutgoing(LogicalMessage logicalMessage, TransportMessage transportMessage)
+        {
+            transportMessage.Headers["Debug"] = Debug.ToString();
+        }
+        
+        static readonly ThreadLocal<bool> debug = new ThreadLocal<bool>();
+
+        public void Customize(BusConfiguration configuration)
+        {
+            configuration.RegisterComponents(c => c.ConfigureComponent<DebugFlagMutator>(DependencyLifecycle.InstancePerCall));
+        }
+    }
+}
