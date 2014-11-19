f547cb2943fee1973c4a47eb623cbe68e64464a7
Ramon Smits <ramon.smits@gmail.com>
2014-11-19 23:38:08 +0100

    Added console titles to more easily identify processes.

--- VideoStore.SqlServer.MultiInstance/VideoStore.Sales/EndpointConfig.cs
+++ VideoStore.SqlServer.MultiInstance/VideoStore.Sales/EndpointConfig.cs
@@ -7,7 +7,8 @@ namespace VideoStore.Sales
     {
         public void Customize(BusConfiguration configuration)
         {
-            // For production use, please select a durable persistence.
+					System.Console.Title = this.GetType().Namespace;
+					// For production use, please select a durable persistence.
             // To use RavenDB, install-package NServiceBus.RavenDB and then use configuration.UsePersistence<RavenDBPersistence>();
             // To use SQLServer, install-package NServiceBus.NHibernate and then use configuration.UsePersistence<NHibernatePersistence>();
             if (Debugger.IsAttached)
