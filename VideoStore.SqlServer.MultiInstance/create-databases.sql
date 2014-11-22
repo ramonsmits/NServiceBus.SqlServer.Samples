7f4f4e50f46c60b7df4dd8acab06834c65952dfc
Ramon Smits <ramon.smits@gmail.com>
2014-11-22 12:15:02 +0100

    SQL helper scripts.

---
+++ VideoStore.SqlServer.MultiInstance/create-databases.sql
@@ -0,0 +1,7 @@
+
+CREATE DATABASE [VideoStore]
+--	CREATE DATABASE [VideoStore.ContentManagement]
+--	CREATE DATABASE [VideoStore.Operations]
+--	CREATE DATABASE [VideoStore.Sales]
+--	CREATE DATABASE [VideoStore.CustomerRelations]
+--	CREATE DATABASE [VideoStore.ECommerce]
---
+++ VideoStore.SqlServer.MultiInstance/drop-tables.sql
@@ -0,0 +1,45 @@
+﻿drop table [VideoStore]..audit
+drop table [VideoStore]..error
+
+
+drop table [VideoStore.ContentManagement]..audit
+drop table [VideoStore.ContentManagement]..error
+drop table [VideoStore.ContentManagement]..[VideoStore.ContentManagement]
+drop table [VideoStore.ContentManagement]..[VideoStore.ContentManagement.RAMON-HOME]
+drop table [VideoStore.ContentManagement]..[VideoStore.ContentManagement.Retries]
+drop table [VideoStore.ContentManagement]..[VideoStore.ContentManagement.Timeouts]
+drop table [VideoStore.ContentManagement]..[VideoStore.ContentManagement.TimeoutsDispatcher]
+
+drop table [VideoStore.CustomerRelations]..audit
+drop table [VideoStore.CustomerRelations]..error
+drop table [VideoStore.CustomerRelations]..[VideoStore.CustomerRelations]
+drop table [VideoStore.CustomerRelations]..[VideoStore.CustomerRelations.RAMON-HOME]
+drop table [VideoStore.CustomerRelations]..[VideoStore.CustomerRelations.Retries]
+drop table [VideoStore.CustomerRelations]..[VideoStore.CustomerRelations.Timeouts]
+drop table [VideoStore.CustomerRelations]..[VideoStore.CustomerRelations.TimeoutsDispatcher]
+
+drop table [VideoStore.ECommerce]..audit
+drop table [VideoStore.ECommerce]..error
+drop table [VideoStore.ECommerce]..[VideoStore.ECommerce]
+drop table [VideoStore.ECommerce]..[VideoStore.ECommerce.RAMON-HOME]
+drop table [VideoStore.ECommerce]..[VideoStore.ECommerce.Retries]
+drop table [VideoStore.ECommerce]..[VideoStore.ECommerce.Timeouts]
+drop table [VideoStore.ECommerce]..[VideoStore.ECommerce.TimeoutsDispatcher]
+
+drop table [VideoStore.Operations]..audit
+drop table [VideoStore.Operations]..error
+drop table [VideoStore.Operations]..[VideoStore.Operations]
+drop table [VideoStore.Operations]..[VideoStore.Operations.RAMON-HOME]
+drop table [VideoStore.Operations]..[VideoStore.Operations.Retries]
+drop table [VideoStore.Operations]..[VideoStore.Operations.Timeouts]
+drop table [VideoStore.Operations]..[VideoStore.Operations.TimeoutsDispatcher]
+
+drop table [VideoStore.Sales]..audit
+drop table [VideoStore.Sales]..error
+drop table [VideoStore.Sales]..[VideoStore.Sales]
+drop table [VideoStore.Sales]..[VideoStore.Sales.RAMON-HOME]
+drop table [VideoStore.Sales]..[VideoStore.Sales.Retries]
+drop table [VideoStore.Sales]..[VideoStore.Sales.Timeouts]
+drop table [VideoStore.Sales]..[VideoStore.Sales.TimeoutsDispatcher]
+
+
